af57095ad169ebfb2de523698223b2aea7871e45
leonardo Prais <leo.junior.meira@gmail.comgit config --listgit config --global user.email leo.junior.meira@gmail.com>
2025-01-22 14:24:54 -0300

    corrigindo o erro do 'year'

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -8,12 +8,23 @@ import com.devsuperior.dslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 	@Query(nativeQuery = true, value = """
-			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
-			tb_game.short_description AS shortDescription, tb_belonging.position
-			FROM tb_game
-			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
-			WHERE tb_belonging.list_id = :listId
-			ORDER BY tb_belonging.position
-				""")
+			SELECT 
+    tb_game.id AS game_id,
+    tb_game.title,
+    tb_game.game_year AS year,
+    tb_game.img_url AS img_url,
+    tb_game.short_description AS short_description,
+    tb_belonging.position 
+FROM 
+    tb_game
+INNER JOIN 
+    tb_belonging 
+ON 
+    tb_game.id = tb_belonging.game_id
+WHERE 
+    tb_belonging.list_id = :listId
+ORDER BY 
+    tb_belonging.position;
+			""")
 	List<GameMinProjection> searchByList(Long listId);
 }
